332ad45ae355ee83a72f3fcd0fd2d3807030dd80
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2023-07-27 21:27:23 +0200

    first commit

---
+++ src/data/jobs.js
@@ -0,0 +1,14 @@
+export const jobs = [{
+    title: 'Frontend web developer at NPAW',
+    url: 'frontend-web-dev-npaw'
+},
+{
+    title: 'Frontend web developer at VASS',
+    url: 'frontend-dev-vass'
+},
+{
+    title: 'Unity developer at Walk3D',
+    url: 'unity-developer-walk3d'
+}]
+
+
